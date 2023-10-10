e302e28c51ffc00abcbfd7a24aa527c06e4cc135
EmanuelNu <emanuel.nu02@gmail.com>
2023-10-10 11:23:56 -0300

    gh-pages for Vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://emaadev.github.io/hoobank",
   server: {
-    host: true
-  }
-})
+    host: true,
+  },
+});
